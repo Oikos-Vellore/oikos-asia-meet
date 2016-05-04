5c975f3cdb578afd67e302d6e11de8b024100873
Abhishek Balaji <abhishek.balaji2@gmail.com>
2016-05-04 13:48:41 +0530

    Added php and connect to heroku

--- signUp.php
+++ signUp.php
@@ -1,29 +1,4 @@
-<?php
-    
-    $name = $_POST['name'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $email = $_POST['email'];
-    $countryResidence = $_POST['country'];
-    $whoAreYou = $_POST['whoAreYou'];
-    $memberType = $_POST['memberType'];
-    $areaOfInterest = $_POST['areaOfInterest'];
-    $learningCircle = $_POST['learningCircle'];
 
-    $con = mysqli_connect("localhost", "root", "root", "test");
-
-    if(!$con)
-    {
-        die "Errorororo" . mysqli_error($con);
-    }
-    else
-    {
-        echo "Huyrrah"
-    }
-    
-
-?>
-
-<!DOCTYPE HTML>
 <html>
             <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico"/>
 <head>
@@ -76,3 +51,36 @@
 				</footer>
 </body>
 </html>
+
+
+<?php
+    
+    $name = $_POST['name'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $email = $_POST['email'];
+    $countryResidence = $_POST['country'];
+    $whoAreYou = $_POST['whoAreYou'];
+    $memberType = $_POST['memberType'];
+    $areaOfInterest = $_POST['areaOfInterest'];
+    $learningCircle = $_POST['learningCircle'];
+
+    $con = mysqli_connect("localhost", "root", "root", "test");
+
+    if(!$con)
+    {
+        die "Errorororo" . mysqli_error($con);
+    }
+    else
+    {
+        echo "Huyrrah";
+    }
+    
+    $query = 'INSERT INTO regInfo ('name', 'phoneNumber', 'email', 'countryResidence', 'whoAreYou', 'memberType', 'areaOfInterest', 'learningCircle') VALUES ('.+.$name.+.', '.+.$phoneNumber.+.', '.+.$email.+.', '.+.$countryResidence.+.', '.+.$whoAreYou.+.', '.+.$memberType.+.', '.+.$areaOfInterest.+.', '.+.$leaningCircle.+.");";
+
+    $insData = mysqli_query($con, $query);
+    if(!$insData)
+        echo "Error";
+    else echo "Insertion successful";
+    
+    $con->close();
+?>
